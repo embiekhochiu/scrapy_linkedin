bd5bc7600fbb6f8b8566156b6e11cecfd9039365
Lan Vy <lanvy181004@gmail.com>
2024-04-03 02:13:21 +0700

    idk

--- linkedin_job/items.py
+++
@@ -1,7 +0,0 @@
-import scrapy
-
-class linkedin_jobItem(scrapy.Item):
-    job_title = scrapy.Field()
-    job_url = scrapy.Field()
-    job_location = scrapy.Field()
-    job_company = scrapy.Field()
--- linkedin_job/pipelines.py
+++
@@ -1,4 +0,0 @@
-class LinkedinJobsPipeline:
-    def process_item(self, item, spider):
-        # Process your items here
-        return item
